b1af8076a3a19a15ecd943ca2160df80a8fe842f
horiz <horiz@Horizon.com>
2024-10-27 18:56:29 -0400

    Add project files.

---
+++ BasicSkiaGUI/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace BasicSkiaGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn10Lines = new System.Windows.Forms.Button();
+            this.btn1kLines = new System.Windows.Forms.Button();
+            this.btn10kLines = new System.Windows.Forms.Button();
+            this.btn100kLines = new System.Windows.Forms.Button();
+            this.imageCanvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn10Lines
+            // 
+            this.btn10Lines.Location = new System.Drawing.Point(12, 12);
+            this.btn10Lines.Name = "btn10Lines";
+            this.btn10Lines.Size = new System.Drawing.Size(75, 23);
+            this.btn10Lines.TabIndex = 0;
+            this.btn10Lines.Text = "10";
+            this.btn10Lines.UseVisualStyleBackColor = true;
+            this.btn10Lines.Click += new System.EventHandler(this.btn10Lines_Click);
+            // 
+            // btn1kLines
+            // 
+            this.btn1kLines.Location = new System.Drawing.Point(93, 12);
+            this.btn1kLines.Name = "btn1kLines";
+            this.btn1kLines.Size = new System.Drawing.Size(75, 23);
+            this.btn1kLines.TabIndex = 1;
+            this.btn1kLines.Text = "1k";
+            this.btn1kLines.UseVisualStyleBackColor = true;
+            this.btn1kLines.Click += new System.EventHandler(this.btn1kLines_Click);
+            // 
+            // btn10kLines
+            // 
+            this.btn10kLines.Location = new System.Drawing.Point(175, 11);
+            this.btn10kLines.Name = "btn10kLines";
+            this.btn10kLines.Size = new System.Drawing.Size(75, 23);
+            this.btn10kLines.TabIndex = 2;
+            this.btn10kLines.Text = "10k";
+            this.btn10kLines.UseVisualStyleBackColor = true;
+            this.btn10kLines.Click += new System.EventHandler(this.btn10kLines_Click);
+            // 
+            // btn100kLines
+            // 
+            this.btn100kLines.Location = new System.Drawing.Point(256, 11);
+            this.btn100kLines.Name = "btn100kLines";
+            this.btn100kLines.Size = new System.Drawing.Size(75, 23);
+            this.btn100kLines.TabIndex = 3;
+            this.btn100kLines.Text = "100k";
+            this.btn100kLines.UseVisualStyleBackColor = true;
+            this.btn100kLines.Click += new System.EventHandler(this.btn100kLines_Click);
+            // 
+            // imageCanvas
+            // 
+            this.imageCanvas.Location = new System.Drawing.Point(12, 41);
+            this.imageCanvas.Name = "imageCanvas";
+            this.imageCanvas.Size = new System.Drawing.Size(812, 480);
+            this.imageCanvas.TabIndex = 4;
+            this.imageCanvas.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 533);
+            this.Controls.Add(this.imageCanvas);
+            this.Controls.Add(this.btn100kLines);
+            this.Controls.Add(this.btn10kLines);
+            this.Controls.Add(this.btn1kLines);
+            this.Controls.Add(this.btn10Lines);
+            this.Name = "Form1";
+            this.Text = "Basic Skia GUI";
+            ((System.ComponentModel.ISupportInitialize)(this.imageCanvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn10Lines;
+        private System.Windows.Forms.Button btn1kLines;
+        private System.Windows.Forms.Button btn10kLines;
+        private System.Windows.Forms.Button btn100kLines;
+        private System.Windows.Forms.PictureBox imageCanvas;
+    }
+}
+
---
+++ BasicSkiaGUI/Form1.cs
@@ -0,0 +1,96 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicSkiaGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rand = new Random();
+
+        private void Render(int lineCount)
+        {
+            SKImageInfo imageInfo = new SKImageInfo(
+                width: imageCanvas.Width, 
+                height: imageCanvas.Height,
+                colorType: SKColorType.Rgba8888,
+                alphaType: SKAlphaType.Premul
+            );
+
+            SKSurface surface = SKSurface.Create( imageInfo );
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear(SKColor.Parse("#003366"));
+
+            for (int i = 0; i < lineCount; i++) 
+            {
+                int minWidth = 1;
+                int maxWidth = 10;
+                float lineWidth = rand.Next(minWidth, maxWidth);
+                SKColor lineColor = new SKColor(
+                    red: (byte)rand.Next(255),
+                    green: (byte)rand.Next(255),
+                    blue: (byte)rand.Next(255),
+                    alpha: (byte)rand.Next(255)
+                );
+                SKPaint linePaint = new SKPaint
+                {
+                    Color = lineColor,
+                    StrokeWidth = lineWidth,
+                    IsAntialias = true,
+                    Style = SKPaintStyle.Stroke
+                };
+                int x1 = rand.Next(imageInfo.Width);
+                int y1 = rand.Next(imageInfo.Height);
+                int x2 = rand.Next(imageInfo.Width);
+                int y2 = rand.Next( imageInfo.Height);
+                canvas.DrawLine(x1, y1, x2, y2, linePaint);
+            }
+
+            using (SKImage image = surface.Snapshot()) {
+                using (SKData data = image.Encode()) {
+                    using (System.IO.MemoryStream mStream = new System.IO.MemoryStream(data.ToArray()))
+                    {
+                        imageCanvas.Image?.Dispose();
+                        imageCanvas.Image = new Bitmap(mStream, false);
+                    }
+                }
+            }
+
+            Application.DoEvents();
+            
+        }
+
+        private void btn10Lines_Click(object sender, EventArgs e)
+        {
+            Render(10);
+        }
+
+        private void btn1kLines_Click(object sender, EventArgs e)
+        {
+            Render(1000);
+        }
+
+        private void btn10kLines_Click(object sender, EventArgs e)
+        {
+            Render(10000);
+        }
+
+        private void btn100kLines_Click(object sender, EventArgs e)
+        {
+            Render(100000);
+        }
+    }
+}
